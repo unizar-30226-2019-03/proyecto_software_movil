51a8a54344247886482f22455d3387ca39228840
LÓPEZ VILLELLAS, LORIEN <738488@celes.unizar.es>
2019-03-22 16:33:50 +0100

    Anyadido un id a cada pantalla

---
+++ src/screens/Inicio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import DownMenu from './src/components/DownMenu/DownMenu';
+
+const Home = (props) => {  
+	return (
+		<Text>TODO: PANTALLA INICIO.</Text>
+	  	<DownMenu/>
+	);
+}
