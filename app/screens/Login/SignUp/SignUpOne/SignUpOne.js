747b63472b7856a6c4f42ef0e9ce0066a2fac6ce
IsaacValdivia <isaac.valdivia.dnc@gmail.com>
2019-03-27 13:37:05 +0100

    Registro terminado

--- app/screens/Login/SignUp/SignUpOne/SignUpOne.js
+++ app/screens/Login/SignUp/SignUpOne/SignUpOne.js
@@ -1,45 +1,98 @@
 import React from "react";
-import { View, Button, TextInput } from "react-native";
-// import MyImagePicker from "../../../../components/MyImagePicker";
+import { View, ActivityIndicator, KeyboardAvoidingView } from "react-native";
+import { Input, Image, Button } from "react-native-elements";
+import { ImagePicker } from "expo";
 
 import styles from "./styles";
 
-export default class SingUpOne extends React.Component {
-	static navigationOptions = ({ navigation }) => ({
-		title: "Registrarse"
-	});
-	render() {
-		return (
-			<View style={styles.container}>
-				{/*				<View style={styles.container07}>
-					<TextInput
-						style={{ borderWidth: 1, paddingHorizontal: 7 }} // TODO: Meterlo en styles
-						onChangeText={text => this.setState({ text })}
-						placeholder={"Usuario*"}
-					/>
-					<TextInput
-						style={{ borderWidth: 1, paddingHorizontal: 7 }} // TODO: Meterlo en styles
-						secureTextEntry={true}
-						onChangeText={text => this.setState({ text })}
-						placeholder={"Contraseña*"}
-					/>
-					<TextInput
-						style={{ borderWidth: 1, paddingHorizontal: 7 }} // TODO: Meterlo en styles
-						secureTextEntry={true}
-						onChangeText={text => this.setState({ text })}
-						placeholder={"Vuelva a introducir la contraseña*"}
-					/>
-					<MyImagePicker />
-					<View
-						style={{
-							flexDirection: "row",
-							justifyContent: "flex-end"
-						}}
-						>
-						<Button title="Siguiente" />
-					</View>
-				</View>*/}
-			</View>
-		);
-	}
+export default class SignUpOne extends React.Component {
+  state = {
+    image: "../../../../assets/splash.png"
+  };
+  static navigationOptions = ({ navigation }) => ({
+    title: "Registrarse"
+  });
+  render() {
+    let { image } = this.state;
+    return (
+      <View style={styles.container}>
+        <View style={styles.logoView}>
+          <Image
+            source={require("../../../../assets/icon.png")}
+            style={{ width: 50, height: 50 }}
+            PlaceholderContent={<ActivityIndicator />}
+          />
+        </View>
+        <View style={styles.container07}>
+          <View style={styles.inputBoxSeparation}>
+            <Input
+              placeholder="Nombre de usuario*"
+              leftIcon={{ type: "font-awesome", name: "user" }}
+              leftIconContainerStyle={styles.inputSeparation}
+            />
+          </View>
+          <View style={styles.inputBoxSeparation}>
+            <Input
+              placeholder="Contraseña*"
+              secureTextEntry={true}
+              leftIcon={{ type: "font-awesome", name: "lock" }}
+              leftIconContainerStyle={styles.inputSeparation}
+            />
+          </View>
+          <View style={styles.inputBoxSeparation}>
+            <Input
+              placeholder="Vuelva a introducir la contraseña*"
+              secureTextEntry={true}
+              leftIcon={{ type: "font-awesome", name: "lock" }}
+              leftIconContainerStyle={styles.inputSeparation}
+            />
+          </View>
+
+          <View
+            style={{
+              paddingTop: 15,
+              flexDirection: "row",
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "space-between"
+            }}
+          >
+            {image && (
+              <Image
+                source={{ uri: image }}
+                style={{ width: 70, height: 70 }}
+              />
+            )}
+            <Button title="Seleccionar foto" onPress={this._pickImage} />
+          </View>
+
+          <View style={styles.description}>
+            <Input
+              placeholder="Escriba una descripción suya"
+              multiline={true}
+            />
+          </View>
+        </View>
+
+        <View style={styles.nextButton}>
+          <Button
+            onPress={() => this.props.navigation.navigate("SignUpTwo")}
+            title="Siguiente"
+          />
+        </View>
+      </View>
+    );
+  }
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3]
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
+  };
 }
--- app/screens/Login/SignUp/SignUpOne/styles.js
+++ app/screens/Login/SignUp/SignUpOne/styles.js
@@ -3,24 +3,37 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
 	container: {
 		flex: 1,
-		paddingHorizontal: 70,
-		justifyContent: "space-around",
+		paddingHorizontal: 30,
+		paddingTop: 20,
+		justifyContent: "flex-start",
 		backgroundColor: "#F5FCFF"
+	},
+	logoView: {
+		paddingTop: 15,
+		paddingBottom: 25,
+		alignItems: "center",
+		justifyContent: "center",
 	},
 	container07: {
 		flex: 0.7,
 		justifyContent: "space-around",
 		backgroundColor: "#F5FCFF"
 	},
-	welcome: {
-		fontSize: 20,
-		backgroundColor: "skyblue",
-		textAlign: "center",
-		margin: 10
+	inputBoxSeparation: {
+		paddingBottom: 25,
 	},
-	instructions: {
-		textAlign: "center",
-		color: "#333333",
-		marginBottom: 5
-	}
+	description: {
+		paddingTop: 10,
+		maxHeight: 20,
+	},
+	inputSeparation: {
+		paddingRight: 10,
+	},
+	nextButton: {
+		position: "absolute",
+		bottom: 10,
+		right: 10,
+		justifyContent: "flex-end",
+		alignItems: "flex-end"
+	},
 });
