652d785d38a1d04f81c75be395f375e70add8101
raquelroy <47817676+raquelroy@users.noreply.github.com>
2019-04-12 00:33:07 +0200

    cambio pantalla biblioteca

--- app/screens/Biblioteca/Biblioteca.js
+++ app/screens/Biblioteca/Biblioteca.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
+import { View } from "react-native";
+import { Text, Button } from "react-native-elements";
+
 
 import styles from "./styles";
 
@@ -7,14 +9,21 @@ export default class Biblioteca extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Button
-					onPress={() =>
-						this.props.navigation.navigate("ListaVideos", {
-							title: "Historial"
-						})
-					}
-					title="IR A HISTORIAL"
-				/>
+				<View style={styles.boton}>
+					<Button
+						onPress={() =>
+							this.props.navigation.navigate("ListaVideos", {
+								title: "Historial"
+							})
+						}
+						title="IR A HISTORIAL"
+						titleStyle={styles.titulo}
+						type="clear"
+						
+					
+					/>
+				</View>
+				<View style={styles.boton}>
 				<Button
 					onPress={() =>
 						this.props.navigation.navigate("ListaVideos", {
@@ -22,15 +31,27 @@ export default class Biblioteca extends React.Component {
 						})
 					}
 					title="IR A MIS VIDEOS"
+					titleStyle={styles.titulo}
+					type="clear"
 				/>
+				</View>
+				<View style={styles.boton}>
 				<Button
 					onPress={() => this.props.navigation.navigate("MisListas")}
 					title="IR A MIS LISTAS"
+					titleStyle={styles.titulo}
+					type="clear"
 				/>
+				</View>
+				<View style={styles.boton}>
 				<Button
 					onPress={() => this.props.navigation.navigate("SubirVideo")}
 					title="IR A SUBIR VIDEO"
+					titleStyle={styles.titulo}
+					type="clear"
+					
 				/>
+				</View>
 			</View>
 		);
 	}
