0e0a0eb780008c6797aedca602986447a33882ab
IsaacValdivia <isaac.valdivia.dnc@gmail.com>
2019-04-15 18:29:12 +0200

    asignatura concreta wip

--- app/screens/Asignaturas/Asignatura/Asignatura.js
+++ app/screens/Asignaturas/Asignatura/Asignatura.js
@@ -1,32 +1,47 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View } from "react-native";
+import { Input, Image, Button } from "react-native-elements";
 
 import { createAppContainer, createStackNavigator } from "react-navigation";
 
 import styles from "./styles";
 
 export default class Asignatura extends React.Component {
-	static navigationOptions = ({ navigation }) => ({
-		title: navigation.getParam("title")
-	});
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam("title")
+  });
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text>TODO ASIGNATURA CONCRETA</Text>
-				<Button
-					onPress={() =>
-						this.props.navigation.navigate("Chat", {
-							title: "Juancho Provisional"
-						})
-					}
-					title="IR A CHAT"
-				/>
-				<Button
-					onPress={() => this.props.navigation.navigate("ViendoVideo")}
-					title="IR A VIDEO"
-				/>
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.viewSeguirAsignatura}>
+          <Button
+            buttonStyle={styles.buttonSeguirAsignatura}
+            title="Seguir asignatura"
+          />
+        </View>
+        <View style={styles.userView}>
+          <Image
+		  // TODO: METER REQUIRE()
+            source={"../../../test/imagenes/perfil.jpg"}
+            style={styles.userIcon}
+            margin={20}
+          />
+          <Text style={styles.userName}>NOMBRE</Text>
+        </View>
+        <Button
+          onPress={() =>
+            this.props.navigation.navigate("Chat", {
+              title: "Juancho Provisional"
+            })
+          }
+          title="IR A CHAT"
+        />
+        <Button
+          onPress={() => this.props.navigation.navigate("ViendoVideo")}
+          title="IR A VIDEO"
+        />
+      </View>
+    );
+  }
 }
--- app/screens/Cuenta/styles.js
+++ app/screens/Cuenta/styles.js
@@ -39,6 +39,5 @@ export default StyleSheet.create({
 		color: 'black',
 		fontWeight: "bold",
 		fontSize: 15,
-		
 	}
 });
