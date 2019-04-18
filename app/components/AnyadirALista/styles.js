37bccaede0fe08df900a85efcf8728a307990a9b
LÓPEZ VILLELLAS, LORIEN <738488@celes.unizar.es>
2019-04-18 18:29:49 +0200

    Estilo modales anayadir a lista y anyadir lista terminado

--- app/components/AnyadirALista/styles.js
+++ app/components/AnyadirALista/styles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 
-import { ScreenhHeight, headerHeight, AzulNuevaLista } from "../../constants";
+import { ScreenhHeight, AzulNuevaLista, headerHeight } from "../../constants";
 
 export default StyleSheet.create({
 	container: {
@@ -8,20 +8,61 @@ export default StyleSheet.create({
 		backgroundColor: "rgba(0, 0, 0, 0.5)"
 	},
 	anyadirAListaContainer: {
+		flex: 1,
 		marginTop: ScreenhHeight * 0.45,
-		height: ScreenhHeight * 0.55,
 		backgroundColor: "#fff"
 	},
 	texto: {
 		fontSize: 18
 	},
+	nuevaListaContainer: {
+		marginLeft: "auto"
+	},
 	nuevaListaTexto: {
 		fontSize: 16,
-		color: AzulNuevaLista,
-		marginLeft: "auto"
+		color: AzulNuevaLista
 	},
 	guardarYNuevaListaContainer: {
 		flexDirection: "row",
 		padding: 10
+	},
+	anyadirListaContainer: {
+		marginTop: ScreenhHeight * 0.1,
+		marginHorizontal: 20,
+		backgroundColor: "#fff"
+	},
+	listoContainer: {
+		padding: 10,
+		flexDirection: "row",
+		alignItems: "center"
+	},
+	listoTexto: {
+		fontSize: 18,
+		marginLeft: 7
+	},
+	listaContainer: {
+		flexDirection: "row",
+		alignItems: "center"
+	},
+	divider: {
+		backgroundColor: "gray"
+	},
+	nuevaListaModalTexto: {
+		padding: 10,
+		marginLeft: 5,
+		fontSize: 18
+	},
+	nuevaListaInputContainer: {
+		marginLeft: 5
+	},
+	cancelar: {
+		marginRight: 10
+	},
+	crearCancelarContainer: {
+		padding: 10,
+		marginTop: 15,
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "flex-end"
 	}
 });
--- app/constants/constants.js
+++ app/constants/constants.js
@@ -16,6 +16,7 @@ export const RojoClaro = "#F90501";
 export const GrisChat = "#ECECEC";
 export const VerdeChat = "#DCF8C6";
 export const AzulNuevaLista = "#2E5FD4";
+export const GrisFondoBarraBusqueda = "#F4F4F4";
 
 export const isIOS = Platform.OS === "ios";
 
