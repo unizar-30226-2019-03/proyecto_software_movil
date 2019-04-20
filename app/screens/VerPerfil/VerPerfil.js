698eaa573309d1b8cdbd4bc5eff3edbb5ef85100
IsaacValdivia <isaac.valdivia.dnc@gmail.com>
2019-04-20 20:51:42 +0200

    Ver perfil propio/ajeno acabado

--- app/screens/VerPerfil/VerPerfil.js
+++ app/screens/VerPerfil/VerPerfil.js
@@ -1,34 +1,122 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
+import { View, TouchableOpacity, ScrollView } from "react-native";
+import { Input } from "react-native-elements";
+import { ImagePicker } from "expo";
 
 import styles from "./styles";
+import ImagenDePerfilConIcono from "../../components/ImagenDePerfilConIcono";
 
 export default class VerPerfil extends React.Component {
-	static navigationOptions = ({ navigation }) => ({
-		title: navigation.getParam("title")
-	});
+  state = {
+    perfilPropioSi: 1,
+    nombre: "Turismundo",
+    descripcion:
+      "Profe tajo guay co\nen plan asi pim pam explicando sistemas empotrados\n\ny otras cosas youknow\njugador profesionaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaal de cricket",
+    imagen:
+      "https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+  };
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text>TODO PERFIL AJENO Y MI PERFIL (todo en 1)</Text>
-				<Button
-					onPress={() =>
-						this.props.navigation.navigate("ModificarCampo", {
-							title: "Escribe tu nombre"
-						})
-					}
-					title="IR A MODIFICAR NOMBRE"
-				/>
-				<Button
-					onPress={() =>
-						this.props.navigation.navigate("ModificarCampo", {
-							title: "Escribe tu descripción"
-						})
-					}
-					title="IR A MODIFICAR DESCRIPCION"
-				/>
-			</View>
-		);
-	}
+  getPerfil = () => {
+    // IMPLEMENTACIÓN API COMPROBAR SI ERES TÚ O ES PERFIL AJENO: this.setState(perfilPropioSi: ...)
+  };
+
+  pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      mediaTypes: "Images",
+      aspect: [4, 4]
+    });
+
+    if (!result.cancelled) {
+      this.setState({ imagen: result.uri });
+    }
+  };
+
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam("title")
+  });
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <View style={styles.viewProfPic}>
+          <ImagenDePerfilConIcono
+            source={this.state.imagen}
+            style={styles.profPic}
+            cambiarSi={!this.state.perfilPropioSi}
+            onPressIcono={this.pickImage}
+          />
+        </View>
+
+        {this.state.perfilPropioSi ? (
+          <View>
+            <TouchableOpacity
+              style={styles.viewNombre}
+              onPress={() =>
+                this.props.navigation.navigate("ModificarCampo", {
+                  title: "Escribe tu nombre"
+                })
+              }
+            >
+              <Input
+                label="Nombre"
+                defaultValue={this.state.nombre}
+                leftIcon={{
+                  type: "font-awesome",
+                  name: "user",
+                  color: "dodgerblue"
+                }}
+                rightIcon={{
+                  type: "font-awesome",
+                  name: "edit",
+                  color: "grey"
+                }}
+                leftIconContainerStyle={styles.leftIconName}
+                rightIconContainerStyle={styles.rightIcon}
+                editable={false}
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.viewDescripcion}
+              onPress={() =>
+                this.props.navigation.navigate("ModificarCampo", {
+                  title: "Escribe tu descripción"
+                })
+              }
+            >
+              <Input
+                label="Descripción"
+                defaultValue={this.state.descripcion}
+                leftIcon={{
+                  type: "font-awesome",
+                  name: "info",
+                  color: "dodgerblue"
+                }}
+                rightIcon={{
+                  type: "font-awesome",
+                  name: "edit",
+                  color: "grey"
+                }}
+                leftIconContainerStyle={styles.leftIconDescr}
+                rightIconContainerStyle={styles.rightIcon}
+                editable={false}
+                multiline={true}
+              />
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.viewNombre}>
+            <Input
+              label="Nombre"
+              defaultValue={this.state.nombre}
+              leftIcon={{ type: "font-awesome", name: "user" }}
+              leftIconContainerStyle={styles.leftIcon}
+              editable={false}
+            />
+          </View>
+        )}
+      </ScrollView>
+    );
+  }
 }
--- app/screens/VerPerfil/styles.js
+++ app/screens/VerPerfil/styles.js
@@ -3,5 +3,38 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
 	container: {
 		flex: 1
+	},
+	viewProfPic: {
+		paddingVertical: 25,
+		alignItems: "center",
+		justifyContent: "center",
+		//borderBottomWidth: 1,
+		//borderColor: "grey",
+	},
+	profPic: {
+		height: 200,
+		aspectRatio: 4/4,
+		borderRadius: 200,
+	},
+	viewNombre: {
+		paddingBottom: 25,
+		alignItems: "flex-start",
+		justifyContent: "center"
+	},
+	viewDescripcion: {
+		alignItems: "flex-start",
+		justifyContent: "center"
+	},
+	leftIconName: {
+		paddingRight: 15,
+		alignSelf: "flex-start",
+	},
+	leftIconDescr: {
+		paddingRight: 20,
+		alignSelf: "flex-start",
+	},
+	rightIcon: {
+		paddingRight: 17,
+		alignSelf: "flex-end",
 	}
 });
