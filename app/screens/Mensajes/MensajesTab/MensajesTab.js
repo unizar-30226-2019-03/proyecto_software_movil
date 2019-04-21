5824b03ad5c1cbdf6234e82baa506e17cf90cf0d
IsaacValdivia <isaac.valdivia.dnc@gmail.com>
2019-04-21 19:54:29 +0200

    Pantalla mensajes/mensajes empezada y acabada

--- app/screens/Mensajes/MensajesTab/MensajesTab.js
+++ app/screens/Mensajes/MensajesTab/MensajesTab.js
@@ -1,22 +1,197 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
+import { Text, TouchableOpacity, ScrollView, View } from "react-native";
+import { Button, Image } from "react-native-elements";
 
 import styles from "./styles";
 
+const samplePic =
+  "https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80";
+
 export default class MensajesTab extends React.Component {
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text>TODO MENSAJES TAB</Text>
-				<Button
-					onPress={() =>
-						this.props.navigation.navigate("Chat", {
-							title: "Juancho Provisional"
-						})
-					}
-					title="IR A CHAT"
-				/>
-			</View>
-		);
-	}
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <TouchableOpacity
+          style={styles.chatContainer}
+          onPress={() =>
+            this.props.navigation.navigate("Chat", {
+              title: "Matilde P."
+            })
+          }
+        >
+          <Image source={{ uri: samplePic }} style={styles.profilePic} />
+          <View style={styles.nameAndMsgContainer}>
+            <Text numberOfLines={1} style={styles.nameText}>
+              Luis Fonsi
+            </Text>
+            <Text numberOfLines={1} style={styles.msgText}>
+              Las notas de aprendizaje automático ya se han subido, puedes
+              consultarlas donde te plazca
+            </Text>
+          </View>
+          <Text style={styles.hourText}>12:58</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.chatContainer}
+          onPress={() =>
+            this.props.navigation.navigate("Chat", {
+              title: "Teodorico"
+            })
+          }
+        >
+          <Image source={{ uri: samplePic }} style={styles.profilePic} />
+          <View style={styles.nameAndMsgContainer}>
+            <Text numberOfLines={1} style={styles.nameText}>
+              Gesaleico Leovigildo López
+            </Text>
+            <Text numberOfLines={1} style={styles.msgText}>
+              Okay brother
+            </Text>
+          </View>
+          <Text style={styles.hourText}>12:58</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.chatContainer}
+          onPress={() =>
+            this.props.navigation.navigate("Chat", {
+              title: "Laura T."
+            })
+          }
+        >
+          <Image source={{ uri: samplePic }} style={styles.profilePic} />
+          <View style={styles.nameAndMsgContainer}>
+            <Text numberOfLines={1} style={styles.nameText}>
+              Inés R.
+            </Text>
+            <Text numberOfLines={1} style={styles.msgText}>
+              Guay
+            </Text>
+          </View>
+          <Text style={styles.hourText}>12:58</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.chatContainer}
+          onPress={() =>
+            this.props.navigation.navigate("Chat", {
+              title: "Laura T."
+            })
+          }
+        >
+          <Image source={{ uri: samplePic }} style={styles.profilePic} />
+          <View style={styles.nameAndMsgContainer}>
+            <Text numberOfLines={1} style={styles.nameText}>
+              Inés R.
+            </Text>
+            <Text numberOfLines={1} style={styles.msgText}>
+              Guay
+            </Text>
+          </View>
+          <Text style={styles.hourText}>12:58</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.chatContainer}
+          onPress={() =>
+            this.props.navigation.navigate("Chat", {
+              title: "Laura T."
+            })
+          }
+        >
+          <Image source={{ uri: samplePic }} style={styles.profilePic} />
+          <View style={styles.nameAndMsgContainer}>
+            <Text numberOfLines={1} style={styles.nameText}>
+              Inés R.
+            </Text>
+            <Text numberOfLines={1} style={styles.msgText}>
+              Guay
+            </Text>
+          </View>
+          <Text style={styles.hourText}>12:58</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.chatContainer}
+          onPress={() =>
+            this.props.navigation.navigate("Chat", {
+              title: "Laura T."
+            })
+          }
+        >
+          <Image source={{ uri: samplePic }} style={styles.profilePic} />
+          <View style={styles.nameAndMsgContainer}>
+            <Text numberOfLines={1} style={styles.nameText}>
+              Inés R.
+            </Text>
+            <Text numberOfLines={1} style={styles.msgText}>
+              Guay
+            </Text>
+          </View>
+          <Text style={styles.hourText}>12:58</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.chatContainer}
+          onPress={() =>
+            this.props.navigation.navigate("Chat", {
+              title: "Laura T."
+            })
+          }
+        >
+          <Image source={{ uri: samplePic }} style={styles.profilePic} />
+          <View style={styles.nameAndMsgContainer}>
+            <Text numberOfLines={1} style={styles.nameText}>
+              Inés R.
+            </Text>
+            <Text numberOfLines={1} style={styles.msgText}>
+              Guay
+            </Text>
+          </View>
+          <Text style={styles.hourText}>12:58</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.chatContainer}
+          onPress={() =>
+            this.props.navigation.navigate("Chat", {
+              title: "Laura T."
+            })
+          }
+        >
+          <Image source={{ uri: samplePic }} style={styles.profilePic} />
+          <View style={styles.nameAndMsgContainer}>
+            <Text numberOfLines={1} style={styles.nameText}>
+              Inés R.
+            </Text>
+            <Text numberOfLines={1} style={styles.msgText}>
+              Guay
+            </Text>
+          </View>
+          <Text style={styles.hourText}>12:58</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.chatContainer}
+          onPress={() =>
+            this.props.navigation.navigate("Chat", {
+              title: "Laura T."
+            })
+          }
+        >
+          <Image source={{ uri: samplePic }} style={styles.profilePic} />
+          <View style={styles.nameAndMsgContainer}>
+            <Text numberOfLines={1} style={styles.nameText}>
+              Inés R.
+            </Text>
+            <Text numberOfLines={1} style={styles.msgText}>
+              Guay
+            </Text>
+          </View>
+          <Text style={styles.hourText}>12:58</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    );
+  }
 }
--- app/screens/Mensajes/MensajesTab/styles.js
+++ app/screens/Mensajes/MensajesTab/styles.js
@@ -3,5 +3,42 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
 	container: {
 		flex: 1
-	}
+	},
+	chatContainer: {
+		marginHorizontal: 8,
+		borderBottomWidth: 1,
+		borderColor: "lightgrey",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between"
+	},
+	profilePic: {
+		width: 50,
+		height: 50,
+		borderRadius: 50,
+		margin: 8,
+	},
+	nameAndMsgContainer: {
+		flexDirection: "column",
+		alignItems: "flex-start",
+		justifyContent: "space-between",
+		width: 250,
+		height: 47,
+	},
+	nameText: {
+		maxWidth: 240,
+		fontWeight: "bold",
+		fontSize: 17
+	},
+	msgText: {
+		maxWidth: 240,
+		color: "darkgrey",
+		fontSize: 15,
+	},
+	hourText: {
+		alignSelf: "flex-start",
+		marginTop: 13,
+		color: "darkgrey",
+		fontSize: 13,
+	},
 });
