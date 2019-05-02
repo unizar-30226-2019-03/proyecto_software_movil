5afa417290de2bd41ee33ecd1b71a757b0ab07dd
LÓPEZ VILLELLAS, LORIEN <738488@celes.unizar.es>
2019-05-02 13:17:57 +0200

    Scroll infinito páginado implementado (en lugar de las asignaturas del usuario salen todas y falta gestionar paginación cuando lo hagan en la api)

--- app/screens/Asignaturas/AsignaturasTab/AsignaturasTab.js
+++ app/screens/Asignaturas/AsignaturasTab/AsignaturasTab.js
@@ -1,42 +1,95 @@
 import React from "react";
-import { View, ListView } from "react-native";
+import { View, FlatList, ActivityIndicator } from "react-native";
+
+import { SubjectApi, ApiClient } from "swagger_unicast";
+
+import { getUserToken } from "../../../config/Auth";
 
 import styles from "./styles";
 
 import ThumbnailAsignatura from "../../../components/ThumbnailAsignatura";
 
 export default class AsignaturasTab extends React.Component {
-  constructor() {
-    super();
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
+  constructor(props) {
+    super(props);
+
     this.state = {
-      dataSource: ds.cloneWithRows([
-        "Asignatura numero 1",
-        "Asignatura mas larga numero 2",
-        "Asignatura mucho mas daawwwwwwwwwwwwwwwmas mas mas larga",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8"
-      ])
+      data: [],
+      loading: true,
+      fetchingNewData: false,
+      refreshing: false,
     };
+
+    this.offset = 1;
+
+    let defaultClient = ApiClient.instance;
+    let bearerAuth = defaultClient.authentications["bearerAuth"];
+    bearerAuth.accessToken = getUserToken();
+
+    this.subjectApiInstance = new SubjectApi();
   }
+
+  getData = () => {
+    this.subjectApiInstance.getSubjects((error, data, response) => {
+      if (!error) {
+        this.offset = this.offset + 1;
+        this.setState({
+          data: [...this.state.data, ...data._embedded.subjects]
+        });
+      }
+    });
+  }
+
+  componentWillMount = () => {
+    this.getData();
+    this.setState({ loading: false })
+  };
+
+  onEndReached = () => {
+    this.setState({ fetchingNewData: true })
+    this.getData();
+    this.setState({ fetchingNewData: false })  
+  }
+
+  onRefresh = () => {
+    this.offset = 1;
+    this.setState({ refreshing: true, data:[] })
+    this.getData();
+    this.setState({ refreshing: false })
+  }
+
+  renderFooter() {
+    return (
+      <View style={styles.footer}>
+        {this.state.fetchingNewData ? (
+          <ActivityIndicator size="large" style={{ marginBottom: 15 }} />
+        ) : null}
+      </View>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={rowData => (
-            <ThumbnailAsignatura
-              navigation={this.props.navigation}
-              icon={require("../../../../test/imagenes/perfil_uni.jpg")}
-              name={rowData}
-            />
-          )}
-        />
+        {this.state.loading ? (
+          <ActivityIndicator size="large" />
+        ) : (
+          <FlatList
+            data={this.state.data}
+            refreshing={this.state.refreshing}
+            onEndReached={() => this.onEndReached()}
+            onRefresh={() => this.onRefresh()}
+            renderItem={({ item }) => (
+              <ThumbnailAsignatura
+                navigation={this.props.navigation}
+                icon={require("../../../../test/imagenes/perfil_uni.jpg")}
+                name={item.name}
+              />
+            )}
+            ListFooterComponent={this.renderFooter.bind(this)}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        )}
       </View>
     );
   }
