8c234f9d75f81ce86d3f209aa682091597a372ed
LÓPEZ VILLELLAS, LORIEN <738488@celes.unizar.es>
2019-05-09 23:42:58 +0200

    Botón seguir asignatura con color correcto

--- app/components/BotonSeguirAsignatura/BotonSeguirAsignatura.js
+++ app/components/BotonSeguirAsignatura/BotonSeguirAsignatura.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Button } from "react-native-elements";
 
+import { TouchableOpacity, Text } from "react-native";
+
+import { Azul } from "../../constants";
+
 import styles from "./styles";
 
 export default class BotonSeguirAsignatura extends React.Component {
@@ -10,14 +14,22 @@ export default class BotonSeguirAsignatura extends React.Component {
 
   render() {
     return (
-      <Button
-        buttonStyle={styles.buttonSeguirAsignatura}
-        title={this.state.asignaturaSeguida ? "Siguiendo" : "Seguir asignatura"}
-        type={this.state.asignaturaSeguida ? "outline" : "solid"}
-        onPress={() =>
-          this.setState({ asignaturaSeguida: !this.state.asignaturaSeguida })
-        }
-      />
+      <TouchableOpacity
+        activeOpacity={1}
+        style={[styles.buttonSeguirAsignatura, { backgroundColor: this.state.asignaturaSeguida ? "white" : Azul }]}
+        onPress={() => this.setState({ asignaturaSeguida: !this.state.asignaturaSeguida })}
+      >
+        <Text style={[styles.text, { color: this.state.asignaturaSeguida ? Azul : "white" }]}>
+          {this.state.asignaturaSeguida ? "Siguiendo" : "Seguir asignatura"}
+        </Text>
+      </TouchableOpacity>
     );
   }
 }
+//  <Button
+//    buttonStyle={[styles.buttonSeguirAsignatura, { backgroundColor: Azul, borderColor: Azul }]}
+// title={this.state.asignaturaSeguida ? "Siguiendo" : "Seguir asignatura"}
+// type={this.state.asignaturaSeguida ? "outline" : "solid"}
+// onPress={() => this.setState({ asignaturaSeguida: !this.state.asignaturaSeguida })}
+
+//  />
--- app/components/BotonSeguirAsignatura/styles.js
+++ app/components/BotonSeguirAsignatura/styles.js
@@ -1,7 +1,17 @@
 import { StyleSheet } from "react-native";
 
+import { Azul } from "../../constants";
+
 export default StyleSheet.create({
-  buttonSeguirAsignatura: {
-    width: 200
-  }
+	buttonSeguirAsignatura: {
+		width: 200,
+		padding: 10,
+		borderRadius: 5,
+		borderWidth: 1.5,
+		borderColor: Azul
+	},
+	text: {
+		fontSize: 17,
+		textAlign: "center"
+	}
 });
