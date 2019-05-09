1048fbcc1e0232897a32abc92e5a593a3ad5b570
LÓPEZ VILLELLAS, LORIEN <738488@celes.unizar.es>
2019-05-09 21:21:56 +0200

    Ahora la flecha hacia atrás se esconde con el resto de controles

--- app/components/VideoConSinFlechaAtras/VideoConSinFlechaAtras.js
+++ app/components/VideoConSinFlechaAtras/VideoConSinFlechaAtras.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
 
 import { Icon } from "react-native-elements";
 
@@ -19,45 +19,84 @@ import styles from "./styles";
  * width, height: altura y anchura del vídeo.
  */
 export default class VideoConSinFlechaAtras extends React.Component {
-  state = {
-    pantallaCompleta: false,
-    orientationChangeSecondCall: false, // onFullscreenUpdate llama dos veces seguidas.
-    posicion: 0,
-    duracion: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      pantallaCompleta: false,
+      orientationChangeSecondCall: false, // onFullscreenUpdate llama dos veces seguidas.
+      posicion: 0,
+      duracion: 0,
+      showControls: false
+    };
+
+    this.controlTimeout = null;
+  }
+
+  showControls = () => {
+    this.clearTimeout();
+    this.setState({ showControls: true });
+    this.setControlTimeout();
   };
 
-  orientationChange() {
+  setControlTimeout = () => {
+    this.controlTimeout = setTimeout(() => {
+      this.setState({ showControls: false });
+    }, 2200);
+  };
+
+  clearTimeout = () => {
+    if (this.controlTimeout != null) {
+      clearTimeout(this.controlTimeout);
+    }
+  };
+
+  clearControlTimeout = () => {
+    this.clearTimeout();
+  };
+
+  componentWillUnmount = () => {
+    this.clearTimeout();
+  };
+
+  orientationChange = () => {
     if (this.state.orientationChangeSecondCall) {
       this.setState({
         orientationChangeSecondCall: false,
         pantallaCompleta: !this.state.pantallaCompleta
       });
 
-      console.log(ScreenOrientation.Orientation.PORTRAIT);
-      console.log(ScreenOrientation.Orientation.LANDSCAPE);
-      this.state.pantallaCompleta
-        ? ScreenOrientation.allowAsync(ScreenOrientation.Orientation.PORTRAIT)
-        : ScreenOrientation.allowAsync(ScreenOrientation.Orientation.LANDSCAPE);
+      if (this.state.pantallaCompleta) {
+        ScreenOrientation.allowAsync(ScreenOrientation.Orientation.PORTRAIT);
+        this.setState({
+          showControls: false
+        });
+      } else {
+        this.clearTimeout();
+        this.setState({
+          showControls: true
+        });
+        ScreenOrientation.allowAsync(ScreenOrientation.Orientation.LANDSCAPE);
+      }
     } else {
       this.setState({
         orientationChangeSecondCall: true
       });
     }
-  }
+  };
 
-  devuelveEstado() {
+  devuelveEstado = () => {
     return this.state.posicion;
-  }
+  };
 
-  devuelveDuracion() {
+  devuelveDuracion = () => {
     return this.state.duracion;
-  }
+  };
 
-  cambio(nuevo) {
+  cambio = nuevo => {
     let posicion = nuevo.positionMillis;
     let duracion = Math.floor(nuevo.durationMillis / 1000 + 0.5);
     this.setState({ posicion: posicion, duracion: duracion });
-  }
+  };
 
   render() {
     const maxWidth = this.props.width == undefined ? ScreenWidth : videoProps.width;
@@ -72,30 +111,32 @@ export default class VideoConSinFlechaAtras extends React.Component {
       videoHeight = videoWidth / screenRatio;
     }
     return (
-      <View style={{ backgroundColor: "black" }}>
-        <Video
-          ref={ref => {
-            this.Video_ref = ref;
-          }}
-          posterSource={{ uri: this.props.thumbnail }}
-          source={{ uri: this.props.source }}
-          rate={1.0}
-          volume={1.0}
-          muted={false}
-          resizeMode="contain"
-          useNativeControls={true}
-          shouldPlay={this.props.autoplay}
-          style={{ width: videoWidth, height: videoHeight }}
-          onFullscreenUpdate={() => this.orientationChange()}
-          progressUpdateIntervalMillis={1000}
-          onPlaybackStatusUpdate={tiempo => this.cambio(tiempo)}
-        />
-        {this.props.flechaSi == true ? (
-          <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={styles.zonaFlechaAtras}>
-            <Icon type="octicon" size={35} name="chevron-left" color="lightgrey" iconStyle={styles.flechaAtras} />
-          </TouchableOpacity>
-        ) : null}
-      </View>
+      <TouchableWithoutFeedback onPress={() => this.showControls()}>
+        <View style={{ backgroundColor: "black" }}>
+          <Video
+            ref={ref => {
+              this.Video_ref = ref;
+            }}
+            posterSource={{ uri: this.props.thumbnail }}
+            source={{ uri: this.props.source }}
+            rate={1.0}
+            volume={1.0}
+            muted={false}
+            resizeMode="contain"
+            useNativeControls={!this.props.flechaSi || (this.props.flechaSi && this.state.showControls)}
+            shouldPlay={this.props.autoplay}
+            style={{ width: videoWidth, height: videoHeight }}
+            onFullscreenUpdate={() => this.orientationChange()}
+            progressUpdateIntervalMillis={1000}
+            onPlaybackStatusUpdate={tiempo => this.cambio(tiempo)}
+          />
+          {this.props.flechaSi && this.state.showControls ? (
+            <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={styles.zonaFlechaAtras}>
+              <Icon type="octicon" size={35} name="chevron-left" color="white" iconStyle={styles.flechaAtras} />
+            </TouchableOpacity>
+          ) : null}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
